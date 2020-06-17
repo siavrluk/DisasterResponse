c7fe10bc79bc4398ea2f537c95e3296d367671d8
silvana <silvana@lukarski.com>
2020-06-17 21:48:31 +0000

    added visualizations

--- app/run.py
+++ app/run.py
@@ -73,7 +73,6 @@ def index():
     
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
@@ -81,8 +80,7 @@ def index():
     categories_count.columns = ['category', 'count']
     categories_count = categories_count.sort_values(by='count', ascending=False)
     
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+
     graphs = [
         {
             'data': [
